763e691ee4c0bfd34cf724bd64ed7e07b1496f79
Nishitharao <31855437+Nishitharao@users.noreply.github.com>
2017-09-12 13:32:26 +0530

    first

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		//Student s=new Student();
+		//Student.Student(1,"sri","1997-08-31",84);
+	}
+
+}
